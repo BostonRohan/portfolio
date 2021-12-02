3228e70b7d9582c5cabf627f5fcfa17fd80670ea
Boston Rohan <bostonrohan@gmail.com>
2021-12-01 19:35:35 -0800

    removed page styles, started animated nav menu

--- src/Components/Navmenu/Nav.js
+++ src/Components/Navmenu/Nav.js
@@ -1,16 +1,40 @@
-import { React } from "react";
-import { useLocation } from "react-router-dom";
-import { NavMenu, NavOnClick } from "./Layout";
+import { React, useState } from "react";
+import { useLocation, useHistory } from "react-router-dom";
+import { Link as Scroll } from "react-scroll";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "./styles.css";
 function Nav() {
-  let location = useLocation();
-  const responsiveNav = () => {
-    if (location.pathname === "/") {
-      return <NavMenu />;
+  const [isOpen, setOpen] = useState(false);
+  const location = useLocation();
+  const history = useHistory();
+  console.log(history);
+  const handleClick = () => {
+    if (location.pathname === "/nav") {
+      setOpen(false);
+      return history.push("/");
     } else {
-      return <NavOnClick />;
+      setOpen(true);
+      return history.goBack();
     }
   };
-  return <>{responsiveNav()}</>;
+  console.log(isOpen);
+  return (
+    <>
+      {/* <Scroll
+        className="logo"
+        activeClass="active"
+        to="Home"
+        spy={true}
+        smooth={true}
+      >
+        Boston
+      </Scroll> */}
+      <div className="nav-icon-container" onClick={handleClick}>
+        <div className={isOpen ? "animate-bar-1" : "bar1"}></div>
+        <div className={isOpen ? "animate-bar-2" : "bar2"}></div>
+        <div className={isOpen ? "animate-bar-3" : "bar3"}></div>
+      </div>
+    </>
+  );
 }
 export default Nav;
