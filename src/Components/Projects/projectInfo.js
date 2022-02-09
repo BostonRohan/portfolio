c98b2c2e6454ca9ca6119947b20f4f3adc930d36
Boston Rohan <bostonrohan@gmail.com>
2022-02-08 19:17:52 -0800

    updated project info

--- src/Components/Projects/projectInfo.js
+++ src/Components/Projects/projectInfo.js
@@ -1,17 +1,28 @@
 export const projectInfo = [
   {
-    videoLink: "Projects/calculator.mp4",
+    images: [],
     description:
-      "Project completed following the FreeCodeCamp curriculum. The project evaluates equations on an iPhone similar calculator. The project also contains light-dark functionality, and mobile responsiveness.",
-    id: "Calculator",
-    repoLink: "https://github.com/BostonRohan/Calculator",
-    liveSiteLink: "https://bostonrohan.github.io/Calculator/",
+      "Dunker is an application for people who love basketball and love to stay updated with the latest news and stats. Dunker allows you to compare your favorite players, check the box score on the day's slate of games, view news from none other than Adrian Wojnarowski, and sign in to take nba quizzes.",
+    id: "dunk",
+    repo: "https://github.com/BostonRohan/dunker",
+    site: "https://dunker.vercel.app/",
     technologies: [
-      "bi-git git-icon",
       "devicon-css3-plain",
       "devicon-react-original",
-      "bi-github github-icon",
+      "devicon-express-original",
+      "devicon-mongodb-plain",
     ],
+    learned:
+      "User authentication, cookie storage, handling an express server, operating CRUD operations. How to handle large amounts of data in a table.",
+  },
+  {
+    images: [],
+    description:
+      "Project completed following the FreeCodeCamp curriculum. The project evaluates equations on an iPhone similar calculator. The project also contains light-dark functionality, and mobile responsiveness.",
+    id: "calc",
+    repo: "https://github.com/BostonRohan/Calculator",
+    site: "https://bostonrohan.github.io/Calculator/",
+    technologies: ["devicon-css3-plain", "devicon-react-original"],
     learned:
       "How to use built-in JavaScript functions in React, basic styling techniques, and how to test for basic bugs and errors in a project.",
   },
