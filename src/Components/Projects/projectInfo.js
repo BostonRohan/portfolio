2fd00ba3c9027ba22c1c45b195aa8864984efa7d
Boston Rohan <bostonrohan@gmail.com>
2022-02-08 20:40:50 -0800

    added images to project info

--- src/Components/Projects/projectInfo.js
+++ src/Components/Projects/projectInfo.js
@@ -1,6 +1,15 @@
 export const projectInfo = [
   {
-    images: [],
+    images: [
+      "./Projects/Dunker/home.png",
+      "./Projects/Dunker/about.png",
+      "./Projects/Dunker/games.png",
+      "./Projects/Dunker/boxscore.png",
+      "./Projects/Dunker/player.png",
+      "./Projects/Dunker/quiz.png",
+      "./Projects/Dunker/score.png",
+      "./Projects/Dunker/news.png",
+    ],
     description:
       "Dunker is an application for people who love basketball and love to stay updated with the latest news and stats. Dunker allows you to compare your favorite players, check the box score on the day's slate of games, view news from none other than Adrian Wojnarowski, and sign in to take nba quizzes.",
     id: "dunk",
@@ -16,7 +25,10 @@ export const projectInfo = [
       "User authentication, cookie storage, handling an express server, operating CRUD operations. How to handle large amounts of data in a table.",
   },
   {
-    images: [],
+    images: [
+      "./Projects/Calculator/white.png",
+      "./Projects/Calculator/black.png",
+    ],
     description:
       "Project completed following the FreeCodeCamp curriculum. The project evaluates equations on an iPhone similar calculator. The project also contains light-dark functionality, and mobile responsiveness.",
     id: "calc",
