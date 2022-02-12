6749f56b2cc3e75b55535be7bbdf3b965ff03d73
Boston Rohan <bostonrohan@gmail.com>
2022-02-11 19:59:54 -0800

    added map

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -25,6 +25,14 @@ function Contact() {
     <div className="Contact">
       <h1>Contact Me</h1>
       {error && <h3 className="error">There was an error, please resubmit.</h3>}
+      <section className="map">
+        <img
+          width="600"
+          height="800"
+          src={`https://maps.geoapify.com/v1/staticmap?style=positron-blue&width=600&height=800&center=lonlat:-122.81638,38.547133&zoom=12.4&pitch=33&marker=lonlat:-122.81436621391315,38.54646663084179;color:%2342445a;size:medium&apiKey=${process.env.REACT_APP_SECRET}`}
+          alt="map"
+        />
+      </section>
       <form ref={form} onSubmit={handleSubmit}>
         <input
           id="name"
