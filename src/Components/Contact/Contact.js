f19c7da91852142d316d63bce79c2d4a248bdcc4
Boston Rohan <bostonrohan@gmail.com>
2022-02-11 20:02:00 -0800

    extended section

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -25,42 +25,42 @@ function Contact() {
     <div className="Contact">
       <h1>Contact Me</h1>
       {error && <h3 className="error">There was an error, please resubmit.</h3>}
-      <section className="map">
+      <section>
         <img
           width="600"
           height="800"
           src={`https://maps.geoapify.com/v1/staticmap?style=positron-blue&width=600&height=800&center=lonlat:-122.81638,38.547133&zoom=12.4&pitch=33&marker=lonlat:-122.81436621391315,38.54646663084179;color:%2342445a;size:medium&apiKey=${process.env.REACT_APP_SECRET}`}
           alt="map"
         />
+        <form ref={form} onSubmit={handleSubmit}>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            placeholder="Name"
+            minLength="3"
+            maxLength="20"
+            required
+          />
+          <input
+            name="email"
+            type="text"
+            placeholder="email"
+            minLength="3"
+            maxLength="254"
+            required
+          />
+          <textarea
+            id="description"
+            name="description"
+            className="contact-description"
+            placeholder="Work with me?"
+            maxLength="200"
+            required
+          />
+          <input className="btn-grad" type="submit" value="Submit" />
+        </form>
       </section>
-      <form ref={form} onSubmit={handleSubmit}>
-        <input
-          id="name"
-          name="name"
-          type="text"
-          placeholder="Name"
-          minLength="3"
-          maxLength="20"
-          required
-        />
-        <input
-          name="email"
-          type="text"
-          placeholder="email"
-          minLength="3"
-          maxLength="254"
-          required
-        />
-        <textarea
-          id="description"
-          name="description"
-          className="contact-description"
-          placeholder="Work with me?"
-          maxLength="200"
-          required
-        />
-        <input className="btn-grad" type="submit" value="Submit" />
-      </form>
     </div>
   );
 }
