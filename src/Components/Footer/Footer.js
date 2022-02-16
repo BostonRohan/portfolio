7f0ed77897e4246b4804eb8e84671d0a241962c2
Boston Rohan <bostonrohan@gmail.com>
2022-02-16 15:49:36 -0800

    updated footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,8 +5,66 @@ import "./styles.css";
 import { openInNewTab } from "../../Utils/openTab";
 function Footer() {
   const main = ["About", "Projects", "Contact"];
-  const projects = ["Dunker, Calculator"];
-  const other = ["Blogs", "School", "Resume", "Hobbies"];
-  return <div className="Footer"></div>;
+  const projects = ["Dunker", "Calculator"];
+  const other = ["Blog", "School", "Resume", "Hobbies"];
+  const year = new Date().getFullYear();
+  return (
+    <footer className="Footer">
+      <div className="title">
+        <h1>Boston Rohan</h1>
+        <section className="copyright">
+          <p>&copy; Copyright 2021-{year}</p>
+          <p>All rights reserved. Created by Boston Rohan.</p>
+        </section>
+      </div>
+      <div className="main">
+        {main.map((title, i) => {
+          return (
+            <Scroll key={i} to={title} smooth={true}>
+              <h3>{title}</h3>
+            </Scroll>
+          );
+        })}
+      </div>
+      <div className="projects">
+        {projects.map((title, i) => {
+          return (
+            <Link key={i} to={`/projects/${title.toLowerCase()}`}>
+              <h3>{title}</h3>
+            </Link>
+          );
+        })}
+        <div className="other">
+          {other.map((title, i) => {
+            return (
+              <Link key={i} to={title.toLowerCase()}>
+                <h3>{title}</h3>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+      <div className="icons">
+        <i
+          className="bi-instagram instagram-icon"
+          onClick={() => openInNewTab("https://www.instagram.com/bosston.r/")}
+        />
+        <i
+          className="bi-linkedin linkedin-icon"
+          onClick={() =>
+            openInNewTab("https://www.linkedin.com/in/bostonrohan/")
+          }
+        />
+        <i
+          className="bi-twitter twitter-icon"
+          onClick={() => openInNewTab("https://twitter.com/BostonRohan")}
+        />
+        <i
+          className="bi-medium medium-icon"
+          onClick={() => openInNewTab("https://medium.com/@bostonrohan")}
+        />
+      </div>
+    </footer>
+  );
 }
 export default Footer;
