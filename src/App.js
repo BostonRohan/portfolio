470d8bc0e4c48143148ff5491a90b1e8183e32fa
Boston Rohan <bostonrohan@gmail.com>
2022-02-23 15:23:34 -0800

    adjusted routes

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ import Blogs from "./Components/Pages/Blog/Blog";
 import School from "./Components/Pages/School/School";
 import Project from "./Components/Pages/Project/Project";
 import ReactSelect from "./Components/Pages/Blog/Blogs/Projects/react-select";
+import Hobbies from "./Components/Pages/Hobbies/Hobbies";
 import "./Universal.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+
 function App() {
   //Light-dark theme
   useEffect(() => {
@@ -20,16 +22,17 @@ function App() {
       <Router>
         <Nav />
         <Switch>
-          <Route path="/" exact component={Main}></Route>
-          <Route path="/blog" exact component={Blogs}></Route>
+          <Route path="/" exact component={Main} />
+          <Route path="/blog" exact component={Blogs} />
           <Route
             path="/blog/projects/learning-react-select"
             exact
             component={ReactSelect}
-          ></Route>
-          <Route path="/school" exact component={School}></Route>
-          <Route path="/projects/:name" exact component={Project}></Route>
-          <Route path="/resume" exact component={Resume}></Route>
+          />
+          <Route path="/school" exact component={School} />
+          <Route path="/projects/:name" exact component={Project} />
+          <Route path="/resume" exact component={Resume} />
+          <Route path="/hobbies" exact component={Hobbies} />
         </Switch>
       </Router>
     </>
