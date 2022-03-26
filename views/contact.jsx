bd6ed5e856f835515c4e319586542da6ada764ac
Boston Rohan <bostonrohan@gmail.com>
2022-03-26 13:34:28 -0700

    adjusted env variables

--- views/contact.jsx
+++ views/contact.jsx
@@ -36,7 +36,7 @@ function Contact() {
           className={styles.img}
           width={600}
           height={800}
-          src={`https://maps.geoapify.com/v1/staticmap?style=positron-blue&width=600&height=800&center=lonlat:-122.81638,38.547133&zoom=12.4&pitch=33&marker=lonlat:-122.81436621391315,38.54646663084179;color:%2342445a;size:medium&apiKey=${process.env.secret}`}
+          src={`https://maps.geoapify.com/v1/staticmap?style=positron-blue&width=600&height=800&center=lonlat:-122.81638,38.547133&zoom=12.4&pitch=33&marker=lonlat:-122.81436621391315,38.54646663084179;color:%2342445a;size:medium&apiKey=${process.env.NEXT_PUBLIC_SECRET}`}
           alt="map"
         />
         <form className={styles.form} ref={form} onSubmit={handleSubmit}>
