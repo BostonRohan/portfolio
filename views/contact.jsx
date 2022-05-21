5dd2c814e80fc683763a9d5a2ed98d102e144a37
Boston Rohan <bostonrohan@gmail.com>
2022-05-20 17:58:53 -0700

    init commit

--- views/contact.jsx
+++ views/contact.jsx
@@ -25,13 +25,6 @@ function Contact() {
   return (
     <div className="Contact">
       <section className={styles.section}>
-        <img
-          className={styles.img}
-          width="600"
-          height="800"
-          src={`https://maps.geoapify.com/v1/staticmap?style=positron-blue&width=600&height=800&center=lonlat:-122.81638,38.547133&zoom=12.4&pitch=33&marker=lonlat:-122.81436621391315,38.54646663084179;color:%2342445a;size:medium&apiKey=${process.env.REACT_APP_SECRET}`}
-          alt="map"
-        />
         <form className={styles.form} ref={form} onSubmit={handleSubmit}>
           <h1 className={styles.h1}>Contact Me</h1>
           {error && (
