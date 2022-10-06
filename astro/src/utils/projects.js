89ab7c31120a3174cb559478fcc84ee8bcc94b8c
Boston Rohan <bostonrohan@gmail.com>
2022-10-06 14:05:50 -0400

    created project section

---
+++ astro/src/utils/projects.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    title: "Spotibot",
+    description: "Spotify Discord Bot.",
+    stack: ["TypeScript", "DiscordJS", "Express", "Prisma"],
+    links: [
+      { type: "github", href: "https://github.com/BostonRohan/spotibot" },
+      { type: "demo", href: "https://github.com/BostonRohan/spotibot" },
+    ],
+  },
+  {
+    title: "Dunker",
+    description: "Basketball stats application.",
+    stack: ["JavaScript", "React", "MongoDB", "Express"],
+    links: [
+      { type: "github", href: "https://github.com/BostonRohan/dunker" },
+      { type: "demo", href: "https://dunker.vercel.app/" },
+    ],
+  },
+  {
+    title: "Calculator",
+    description: "iPhone calculator.",
+    stack: ["React"],
+    links: [
+      {
+        type: "github",
+        href: "https://github.com/BostonRohan/calculator",
+      },
+      { type: "demo", href: "https://bostonrohan.github.io/calculator/" },
+    ],
+  },
+];
