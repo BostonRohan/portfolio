d14bafa7ed7c1fe3fc1bc4191548f94a290412c8
BostonRohan <bostonrohan@gmail.com>
2023-09-24 15:53:43 -0400

    adjust blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-
 import sitemap from "@astrojs/sitemap";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://bostonrohan.com",
-  integrations: [tailwind(), preact(), sitemap()],
+  integrations: [tailwind(), preact(), sitemap(), mdx()]
 });
