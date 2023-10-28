48933e5f1f08a47f40c7b971e08e291a1c91dcee
BostonRohan <bostonrohan@gmail.com>
2023-10-28 13:23:07 -0400

    add reading time to blog card

--- src/utils/blogs.js
+++ src/utils/blogs.js
@@ -3,5 +3,6 @@ export default [
     title: "Learning React Select",
     body: "Being a new web developer is hard. Having to come up with your own projects, working day and night to learn new technologies, the process can become stressful. Personally, I am thankful to have positive influences in the space that I can rely on for advice. Nevertheless, we are all learning, and many of us are in different stages of that process. Though I am still in the beginning process of my learning, I hope to offer some insight that is helpful. Anyways, onto React Select!",
     date: "November 29, 2021",
+    readTime: "5 min"
   },
 ];
