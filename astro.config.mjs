5041eb53388a92a014c7e750876456e85342dde1
BostonRohan <bostonrohan@gmail.com>
2025-01-25 18:18:29 -0500

    feat: add contact button in header, add svelte for later and dynamic render of icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,14 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 
+import svelte from "@astrojs/svelte";
+
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://bostonrohan.com",
-  integrations: [sitemap(), mdx()],
+  integrations: [sitemap(), mdx(), svelte(), icon()],
   vite: {
     plugins: [tailwindcss()],
   },
