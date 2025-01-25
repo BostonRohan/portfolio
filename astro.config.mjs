7c35f498e7514a9de25c2f776a0664d2b02c6cae
BostonRohan <bostonrohan@gmail.com>
2025-01-25 16:38:02 -0500

    feat: update packages
    astro, and tailwindv4
    
    chore: update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://bostonrohan.com",
-  integrations: [tailwind(), sitemap(), mdx()],
+  integrations: [sitemap(), mdx()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
